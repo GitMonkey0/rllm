993fc44173c9071db78e657667125ae684d9c7bb
luhaotian <luhaotian@bytedance.com>
2025-11-14 14:54:32 +0800

    debug

--- rllm/trainer/verl/agent_ppo_trainer.py
+++ rllm/trainer/verl/agent_ppo_trainer.py
@@ -181,7 +181,7 @@ class AgentPPOTrainer(RayPPOTrainer):
                         final_gen_batch_output, generate_metrics = self.generate_agent_trajectory(timing_raw=timing_raw, meta_info=batch.meta_info)
                         batch = batch.union(final_gen_batch_output)
                         metrics.update(generate_metrics)
-
+                    print(self.tokenizer.decode(batch.batch["input_ids"][0], skip_special_tokens=False))
                     # compute values
                     if self.use_critic:
                         with marked_timer("values", timing_raw):
